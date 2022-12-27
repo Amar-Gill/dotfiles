5e4c9ae3e70223e0e73108363c0976f08939c4c9
Amar Gill <amardeep@clearbanc.com>
2022-12-26 21:19:19 -0500

    nvim: refactor lsp server configs

--- nvim/lua/amar/lsp/servers.lua
+++ nvim/lua/amar/lsp/servers.lua
@@ -4,78 +4,57 @@ if not status then
 end
 
 local servers = {
-	"sumneko_lua",
-	"html",
-	"tsserver",
-	"pyright",
-	"bashls",
-	"tailwindcss",
-	"gopls",
+	sumneko_lua = {
+		settings = {
+			Lua = {
+				diagnostics = {
+					-- get the language server to recognize the "vim" global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				completion = {
+					callSnippet = "Replace",
+				},
+				telemetry = {
+					enable = false,
+				},
+			},
+		},
+	},
+	html = {},
+	tsserver = {},
+	pyright = {},
+	bashls = {
+		filetypes = { "sh", "zsh", "bash" },
+	},
+	tailwindcss = {},
+	gopls = {},
 }
 
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = servers,
+	ensure_installed = vim.tbl_keys(servers), -- TODO is this option required also?
 	automatic_installation = { exclude = { "groovyls" } },
 })
 
 local on_attach = require("amar.lsp.handlers").on_attach
 local capabilities = require("amar.lsp.handlers").capabilities
 
-nvim_lsp.tsserver.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
+for server, server_opts in pairs(servers) do
+	local base_opts = {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	}
 
-nvim_lsp.html.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
+	-- "keep" means server_opts is prioritized over base_opts if there is key name collision
+	local opts = vim.tbl_deep_extend("keep", server_opts, base_opts)
 
-nvim_lsp.tailwindcss.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
-
-nvim_lsp.sumneko_lua.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- get the language server to recognize the "vim" global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			completion = {
-				callSnippet = "Replace",
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
-})
-
-nvim_lsp.pyright.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
-
-nvim_lsp.gopls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-})
-
-nvim_lsp.bashls.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "sh", "zsh", "bash" },
-})
+	nvim_lsp[server].setup(opts)
+end
 
 -- special cases
 local groovyls_dir = os.getenv("HOME") .. "/groovy-language-server/build/libs/groovy-language-server-all.jar"

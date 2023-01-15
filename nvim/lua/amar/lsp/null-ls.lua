264f974e50b0a0db3efa04c16d72423a8fe54a5c
Amar Gill <amardeep@clearbanc.com>
2023-01-14 19:21:35 -0500

    nvim: add standardrb formatting and diagnostics

--- nvim/lua/amar/lsp/null-ls.lua
+++ nvim/lua/amar/lsp/null-ls.lua
@@ -16,9 +16,11 @@ null_ls.setup({
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.beautysh.with({ extra_args = { "--indent-size", "2" } }),
 		formatting.gofumpt,
+		formatting.standardrb,
 		diagnostics.eslint_d,
 		diagnostics.flake8,
 		diagnostics.golangci_lint,
+		diagnostics.standardrb,
 		-- not installed with mason
 		formatting.npm_groovy_lint,
 		diagnostics.tidy,

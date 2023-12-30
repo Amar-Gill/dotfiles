4cf68a3731a6cbe4a7a77b3e8ff7a07cd3120793
Amar Gill <amar.s.gill@outlook.com>
2023-12-29 22:44:06 -0500

    nvim: separate completions plugin spec

---
+++ nvim/lua/plugins/completion.lua
@@ -0,0 +1,12 @@
+return {
+	"hrsh7th/nvim-cmp", -- The completion plugin
+	"hrsh7th/cmp-nvim-lsp", -- lsp completion source
+	"hrsh7th/cmp-buffer", -- buffer completions
+	"hrsh7th/cmp-path", -- path completions
+	"saadparwaiz1/cmp_luasnip", -- snippet completions
+	"hrsh7th/cmp-cmdline", -- command line completions
+
+	-- Snippets
+	"L3MON4D3/LuaSnip",
+	"rafamadriz/friendly-snippets",
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -24,17 +24,6 @@ return {
 	"williamboman/mason-lspconfig.nvim", -- external dependencies manager
 	{ "j-hui/fidget.nvim" }, -- LSP status updates
 
-	-- completion
-	"hrsh7th/nvim-cmp", -- The completion plugin
-	"hrsh7th/cmp-nvim-lsp", -- lsp completion source
-	"hrsh7th/cmp-buffer", -- buffer completions
-	"hrsh7th/cmp-path", -- path completions
-	"saadparwaiz1/cmp_luasnip", -- snippet completions
-	"hrsh7th/cmp-cmdline", -- command line completions
-
-	-- Snippets
-	"L3MON4D3/LuaSnip",
-	"rafamadriz/friendly-snippets",
 
 	-- treesitter (syntax parsing)
 	{

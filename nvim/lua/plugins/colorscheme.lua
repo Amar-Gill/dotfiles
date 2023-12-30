0fcc4a0a87869a43a534df8beb0135e174a466d5
Amar Gill <amar.s.gill@outlook.com>
2023-12-29 22:55:00 -0500

    nvim: separate colorscheme plugin spec

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,7 @@
+return {
+	"lewpoly/sherbet.nvim",
+	"olivercederborg/poimandres.nvim",
+	"mhartington/oceanic-next",
+	"sainnhe/edge",
+	{ "rose-pine/neovim", name = "rose-pine" },
+}
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,13 +1,6 @@
 return {
 	"nvim-lua/plenary.nvim", -- Common utilities
 
-	-- color themes
-	"lewpoly/sherbet.nvim",
-	"olivercederborg/poimandres.nvim",
-	"mhartington/oceanic-next",
-	"sainnhe/edge",
-	{ "rose-pine/neovim", name = "rose-pine" },
-
 	-- status line
 	"nvim-lualine/lualine.nvim",
 

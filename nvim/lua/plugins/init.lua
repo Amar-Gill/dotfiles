f410ec65a12af0881f61e434b52e631a3943b949
Amar Gill <amar.s.gill@outlook.com>
2024-02-08 21:06:21 -0500

    nvim: add peek.nvim plugin

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -31,4 +31,17 @@ return {
 	"github/copilot.vim",
 
 	"folke/zen-mode.nvim",
+
+	-- markdown preview
+	{
+		"toppair/peek.nvim",
+		event = { "VeryLazy" },
+		build = "deno task --quiet build:fast",
+		config = function()
+			require("peek").setup()
+			-- refer to `configuration to change defaults`
+			vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+		end,
+	},
 }

7d28e6f02ba474926dbe649b1c2b57fd730cab9c
Amar Gill <amar.s.gill@outlook.com>
2024-02-10 16:26:12 -0500

    nvim: mv autopairs config into lazy spec

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,6 +1,29 @@
 return {
 	-- auto close tags and brackets / parentheses
-	{ "windwp/nvim-autopairs", event = "InsertEnter" },
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {
+			check_ts = true,
+			ts_config = {
+				lua = { "string", "source" },
+				javascript = { "string", "template_string" },
+				java = false,
+			},
+			disable_filetype = { "TelescopePrompt", "vim", "spectre_panel" },
+			fast_wrap = {
+				map = "<M-w>",
+				chars = { "{", "[", "(", '"', "'" },
+				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+				offset = 0, -- Offset from pattern match
+				end_key = "$",
+				keys = "qwertyuiopzxcvbnmasdfghjkl",
+				check_comma = true,
+				highlight = "PmenuSel",
+				highlight_grey = "LineNr",
+			},
+		},
+	},
 
 	-- auto conversion of f strings (js/ts and python)
 	{

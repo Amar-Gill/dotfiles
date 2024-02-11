daba1a63b5f233e1661bd64545b93bf45d375ecc
Amar Gill <amar.s.gill@outlook.com>
2024-02-11 13:24:34 -0500

    nvim: configure completion plugin spec

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -1,14 +1,20 @@
 return {
-	"hrsh7th/nvim-cmp", -- The completion plugin
-	"hrsh7th/cmp-nvim-lsp", -- lsp completion source
-	"hrsh7th/cmp-buffer", -- buffer completions
-	"hrsh7th/cmp-path", -- path completions
-	"saadparwaiz1/cmp_luasnip", -- snippet completions
-	"hrsh7th/cmp-cmdline", -- command line completions
+	-- The completion plugin
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+
+			"hrsh7th/cmp-nvim-lsp", -- lsp completion source
+			"hrsh7th/cmp-buffer", -- buffer completions
+			"hrsh7th/cmp-path", -- path completions
+			"hrsh7th/cmp-cmdline", -- command line completions
+		},
+	},
 
 	-- Snippets
 	"L3MON4D3/LuaSnip",
 	"rafamadriz/friendly-snippets",
+	"saadparwaiz1/cmp_luasnip", -- snippet completions
 
 	-- copilot
 	"github/copilot.vim",

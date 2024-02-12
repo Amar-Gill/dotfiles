89eb9b653c3db62b14c14c670bf694dab0044857
Amar Gill <amar.s.gill@outlook.com>
2024-02-12 00:26:03 -0500

    nvim: mv plenary into telescope dependencies

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
+			"nvim-lua/plenary.nvim", -- Common utilities
 			"nvim-telescope/telescope-file-browser.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", enabled = vim.fn.executable("make") == 1 },
 		},

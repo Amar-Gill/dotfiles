3274c1165c656cb8c6ebcce82a4afc9261450792
Amar Gill <amar.s.gill@outlook.com>
2024-02-12 00:14:21 -0500

    nvim: rework telescope plugin spec

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,9 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	"nvim-telescope/telescope-file-browser.nvim",
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", enabled = vim.fn.executable("make") == 1 },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-telescope/telescope-file-browser.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", enabled = vim.fn.executable("make") == 1 },
+		},
+	},
 }

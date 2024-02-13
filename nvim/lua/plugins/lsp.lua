c9f5ee2d080ad7aa7bde873eb4f8279cd56e332c
Amar Gill <amar.s.gill@outlook.com>
2024-02-12 22:00:32 -0500

    nvim: restructure lsp plugins spec

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,12 +1,16 @@
 return {
-	"neovim/nvim-lspconfig", -- enable lsp
-	"nvimtools/none-ls.nvim",
-	"ray-x/lsp_signature.nvim", -- show lsp signature in insert mode when calling a function
-	"folke/neodev.nvim", -- annotations for vim and plugins (requires sumneko_lua ls)
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim", -- external dependencies manager
-	{ "j-hui/fidget.nvim" }, -- LSP status updates
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"nvimtools/none-ls.nvim",
+			"ray-x/lsp_signature.nvim", -- show lsp signature in insert mode when calling a function
+			"folke/neodev.nvim", -- annotations for vim and plugins (requires lua ls)
+			"williamboman/mason.nvim", -- external dependencies manager
+			"williamboman/mason-lspconfig.nvim",
+			"j-hui/fidget.nvim", -- LSP status updates
+		},
+	},
 
 	-- breadcrumbs
-	"SmiteshP/nvim-navic",
+	{ "SmiteshP/nvim-navic", lazy = true },
 }

7377eaa27f0af3eb0da9d0b98e2f102b14fd57fa
Amar Gill <amar.s.gill@outlook.com>
2024-02-12 20:41:44 -0500

    nvim: update treesitter plugin specs

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,13 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			"windwp/nvim-ts-autotag",
+		},
 	},
-	"JoosepAlviste/nvim-ts-context-commentstring",
-	"windwp/nvim-ts-autotag",
 
 	-- syntax highlighting special cases
-	"towolf/vim-helm", -- highlighting for helm charts which are Go templates disguised as .yaml files
-	"Cian911/vim-cadence", -- cadence language (flow smart contracts)
+	{ "towolf/vim-helm" }, -- highlighting for helm charts which are Go templates disguised as .yaml files
+	{ "Cian911/vim-cadence" }, -- cadence language (flow smart contracts)
 }

43d4d3d95a3a265e033f37547edc5a4885ad7f6d
Amar Gill <amar.s.gill@outlook.com>
2024-02-12 19:34:08 -0500

    nvim: rm /lua/amar/colors module

--- nvim/lua/amar/colors.lua
+++
@@ -1,9 +0,0 @@
-vim.o.background = "dark" -- or "light" for light mode
-
-local colorscheme = "rose-pine"
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-	vim.notify("colorscheme " .. colorscheme .. " not found!")
-	return
-end

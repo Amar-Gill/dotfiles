c3674aa70f4b8074fea449a8c4b440ba425a4231
Amar Gill <amar.s.gill@outlook.com>
2024-03-02 21:23:00 -0500

    nvim_lsp: rm formatting builtins for null-ls

--- nvim/lua/amar/config/lsp/null-ls.lua
+++ nvim/lua/amar/config/lsp/null-ls.lua
@@ -1,7 +1,5 @@
 local null_ls = require("null-ls")
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
@@ -9,14 +7,8 @@ null_ls.setup({
 	debug = false,
 	border = "rounded",
 	sources = {
-		formatting.prettierd.with({ extra_filetypes = { "astro", "svelte" } }),
-		formatting.stylua,
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.gofumpt,
 		diagnostics.golangci_lint,
 		-- not installed with mason
-		formatting.npm_groovy_lint,
-		formatting.mix,
 		diagnostics.tidy,
 		diagnostics.credo,
 	},

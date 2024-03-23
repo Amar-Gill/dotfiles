4c97258dd78892d9656f6f33bba7cf753db0f944
Amar Gill <amar.s.gill@outlook.com>
2024-03-23 14:11:23 -0400

    nvim: add group to ColorScheme autocmd

--- nvim/lua/amar/autocmds.lua
+++ nvim/lua/amar/autocmds.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_create_autocmd("ColorScheme", {
 	pattern = "*",
 	desc = "Avoid overwritten by loading color schemes later",
+	group = vim.api.nvim_create_augroup("colorscheme-float-window-hl-normalize", { clear = true }),
 	callback = function()
 		vim.api.nvim_set_hl(0, "NormalFloat", {
 			link = "Normal",

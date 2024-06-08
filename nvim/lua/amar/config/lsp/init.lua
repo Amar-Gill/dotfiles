348e9c4280afb2763290467b42e3b5c2dbbc0eb4
Amar Gill <amar.s.gill@outlook.com>
2024-06-08 14:48:35 -0400

    nvim: rm neodev plugin because EOL

--- nvim/lua/amar/config/lsp/init.lua
+++ nvim/lua/amar/config/lsp/init.lua
@@ -1,4 +1,3 @@
-require("neodev").setup() -- must be configured before sumneko_lua ls
 require("amar.config.lsp.servers")
 require("amar.config.lsp.diagnostic-config")
 require("amar.config.lsp.null-ls")
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,14 +4,13 @@ return {
 		dependencies = {
 			"nvimtools/none-ls.nvim",
 			"ray-x/lsp_signature.nvim", -- show lsp signature in insert mode when calling a function
-			"folke/neodev.nvim", -- annotations for vim and plugins (requires lua ls)
 			"williamboman/mason.nvim", -- external dependencies manager
 			"williamboman/mason-lspconfig.nvim",
 			"j-hui/fidget.nvim", -- LSP status updates
 		},
-    config = function()
-      require("amar.config.lsp")
-    end
+		config = function()
+			require("amar.config.lsp")
+		end,
 	},
 
 	-- breadcrumbs

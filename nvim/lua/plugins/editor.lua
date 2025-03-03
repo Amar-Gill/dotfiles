a5fe41dcfab0fe459949aa36902e1385d5f5e219
Amar Gill <amar.s.gill@outlook.com>
2025-03-03 01:22:37 -0500

    nvim: update lualine separators

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -5,8 +5,8 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
+				component_separators = { left = "🯖", right = "" },
+				section_separators = { left = "🭛", right = "🭋" },
 				disabled_filetypes = {
 					statusline = {},
 					winbar = {},

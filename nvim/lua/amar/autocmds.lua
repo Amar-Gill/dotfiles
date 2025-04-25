11f25c092ca7da117c363ac5004de429eaeac59b
Amar Gill <amar.s.gill@outlook.com>
2025-04-24 23:45:16 -0400

    nvim: delete colorscheme autocmd

--- nvim/lua/amar/autocmds.lua
+++ nvim/lua/amar/autocmds.lua
@@ -1,17 +1,3 @@
-vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	desc = "Avoid overwritten by loading color schemes later",
-	group = vim.api.nvim_create_augroup("colorscheme-float-window-hl-normalize", { clear = true }),
-	callback = function()
-		vim.api.nvim_set_hl(0, "NormalFloat", {
-			link = "Normal",
-		})
-		vim.api.nvim_set_hl(0, "FloatBorder", {
-			link = "Normal",
-		})
-	end,
-})
-
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
